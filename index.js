342e32ecb73f524d1fddf16c839ef41843b4b3d5
andresbernal96 <andresfbernalm@hotmail.com>
2017-09-08 03:00:56 -0500

    final

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ var server      = require('http').createServer(app);
 var io          = require('socket.io')(server);
 
 io.on('connection',function(client){
-    console.log("Client connected...");
-    
-    client.on("join", function(){
-        console.log("Ingresado nuevo cliente");
-    });
-
-    client.on("mensajeSocket",function(mensaje){
-        console.log("Mensaje: "+mensaje);
+ client.on("mensajeSocket",function(mensaje){
         client.broadcast.emit("enviarMensaje",mensaje);
     });
 });
