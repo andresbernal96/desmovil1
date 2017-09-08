8f97961433e989a961abac7a6df3c3b437a761e2
andresbernal96 <andresfbernalm@hotmail.com>
2017-09-07 20:51:44 -0500

    cambios en el servido

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var server = require('http').createServer(app);
+var express     = require('express');
+var app         = express();
+var server      = require('http').createServer(app);
+var io          = require('socket.io')(server);
 
-io.on('connection', function (socket) {
-  console.log("Client connected...");
+io.on('connection',function(client){
+    console.log("Client connected...");
+    
+    client.on("join", function(){
+        console.log("Ingresado nuevo cliente");
+    });
 
-  client.on("mensajeSocket", function (mensaje) {
-    console.log("Mensaje: " + mensaje);
-    client.broadcast.emit("enviarMensaje", mensaje);
-  });
+    client.on("mensajeSocket",function(mensaje){
+        console.log("Mensaje: "+mensaje);
+        client.broadcast.emit("enviarMensaje",mensaje);
+    });
 });
 
 server.listen(process.env.PORT || 3000);
+
