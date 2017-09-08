872f8f6d9799bff969f4cab7f5601ec7bde9784a
andresbernal96 <andresfbernalm@hotmail.com>
2017-09-07 20:50:17 -0500

    cambios en el servidor

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io= require('socket.io')(http);
-var server= require('http').createServer(app);
-port= process.env.PORT || 3000;
+var io = require('socket.io')(http);
+var server = require('http').createServer(app);
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log("Client connected...");
-  
-    client.on("mensajeSocket",function(mensaje){
-      console.log("Mensaje: "+mensaje);
-      client.broadcast.emit("enviarMensaje",mensaje);
+
+  client.on("mensajeSocket", function (mensaje) {
+    console.log("Mensaje: " + mensaje);
+    client.broadcast.emit("enviarMensaje", mensaje);
   });
-  });
-  
-server.listen(port, function(){
-  console.log('listening on *:3000');
 });
+
+server.listen(process.env.PORT || 3000);
